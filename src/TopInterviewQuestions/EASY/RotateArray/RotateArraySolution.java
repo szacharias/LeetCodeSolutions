36fb8d4f25c221b95fae6cca33291a45d6a62ce9
Matthew <matthewsah@hotmail.com>
2019-07-12 12:01:27 +0800

    #init RotateArray

---
+++ src/TopInterviewQuestions/EASY/RotateArray/RotateArraySolution.java
@@ -0,0 +1,14 @@
+package TopInterviewQuestions.EASY.RotateArray;
+
+public class RotateArraySolution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 
+	}
+	public void rotate(int[] nums, int k ) {
+		
+	}
+	
+}
