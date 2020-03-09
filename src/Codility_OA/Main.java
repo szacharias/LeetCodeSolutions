aaadc0c6df54f7f3808b231dd7f1f67e5c0cc097
Sah <matthewsah@hotmail.com>
2020-03-09 12:20:05 -0400

    #0309 update OAs

--- src/Codility_OA/Main.java
+++ src/Codility_OA/Main.java
@@ -4,9 +4,35 @@ package Codility_OA;
 public class Main {
 
 	public static void main(String[] args) { 
-		int A = 3;
+//		int A = 0;
+//		int B = 1;
+//		int C = 8; 
+		// 5
+
+//		int A = 6;
+//		int B = 1;
+//		int C = 1; 
+		// 8 
+
+//		int A = 1;
+//		int B = 3;
+//		int C = 1;
+		//   5 digit
+
+//		int A = 5;
+//		int B = 1;
+//		int C = 1;
+		//7
+		
+//		int A = 0;
+//		int B = 0;
+//		int C = 0;
+		// 2
+
+		int A = 4;
 		int B = 3;
-		int C = 1; 
+		int C = 0;
+		
 		Solution solution = new Solution();
 //		System.out.println(solution.solution(A,B,C));
 		String return_answer = solution.solution(A,B,C);
--- src/Codility_OA/Solution.java
+++ src/Codility_OA/Solution.java
@@ -4,59 +4,72 @@ import java.util.ArrayList;
 public class Solution {
 
 	public String solution(int A, int B,int C ){
+		
+		// Init Vars
 		boolean foundResults = false;
 	    int numArr[] = {A,B,C};
 	    String returnString = ""; 
 	    String previousKey = "";  
+	    int consecutiveStringsA = 0;
+	    int consecutiveStringsB = 0;
+	    int consecutiveStringsC = 0; 
+	    boolean notA = false;
+	    boolean notB = false;
+	    boolean notC = false;
+	    
+	    // Choose next letter
 	    while( foundResults == false) { 
 	    	int max = Math.max(A , Math.max(B, C));  
 	    	
 	    	// Decide on the Character to use
-	    	if (previousKey == "A" && max == A) { 
+	    	if ( (previousKey == "A" && max == A ) && consecutiveStringsA == 2  ) { 
+	    		notA = true;
 		    	max = Math.max( B, C); 
-	    	}else if (previousKey == "B" && max == B) { 
+		    	consecutiveStringsA = 0;
+	    	}else if (previousKey == "B" && max == B && consecutiveStringsB == 2  ) { 
+	    		notB = true;
 		    	max = Math.max( A, C); 
-	    	}else if (previousKey == "C" && max == C) { 
+		    	consecutiveStringsB = 0;
+	    	}else if (previousKey == "C" && max == C && consecutiveStringsC == 2  ) { 
+	    		notC = true;
 		    	max = Math.max( A, B); 
-	    	}
+		    	consecutiveStringsC = 0;
+	    	} 
 	    	if (max == 0) {
 	    		foundResults = true;
 	    		return returnString;
 	    	}
 	    	
-	    	
-	    	// Decide on how many characters to use
-	    	if (max == A && previousKey !="A" ) {
-	    		int tester = max %2 ;
-	    		if (tester == 1) {
-	    			returnString = returnString + "a";
-	    			A = A -1;
-	    		}else {
-	    			returnString = returnString + "aa";
-	    			A = A - 2;
-	    		}
+	    	// Decide on how many characters to use and insert
+	    	if (max == A  && notA == false) {   
+    			returnString = returnString + "a";
+    			A = A -1; 
 	    		previousKey = "A";
+	    		consecutiveStringsA +=1;
+	    	    consecutiveStringsB = 0;
+	    	    consecutiveStringsC = 0; 
+	    		notB = false;
+	    		notC = false;
 	    		
-	    	}else if (max == B  && previousKey !="B" ) {
-	    		int tester = max %2 ;
-	    		if (tester == 1) {
-	    			returnString = returnString + "b";
-	    			B = B -1;
-	    		}else {
-	    			returnString = returnString + "bb";
-	    			B = B -2;
-	    		}
+	    	}else if (max == B  && notB == false ) {
+	    		returnString = returnString + "b";
+    			B = B -1; 
 	    		previousKey = "B";
-	    	}else if (max == C  && previousKey !="C" ) { 
-	    		int tester = max %2 ;
-	    		if (tester == 1) {
-	    			C = C -1 ;
-	    			returnString = returnString + "c";
-	    		}else {
-	    			returnString = returnString + "cc";
-	    			C = C -2;
-	    		}
+	    		consecutiveStringsA = 0;
+	    	    consecutiveStringsB +=1 ;
+	    	    consecutiveStringsC = 0; 
+	    		notA = false;
+	    		notC = false;
+	    	}else if (max == C && notC == false  ) {  
+	    		returnString = returnString + "c";
+    			C = C -1; 
 	    		previousKey = "C";
+	    		consecutiveStringsA = 0;
+	    	    consecutiveStringsB = 0 ;
+	    	    consecutiveStringsC += 1; 
+	    		notB = false;
+	    		notA = false;
+	    		
 	    	}
 	    	
 	    	if (Math.max(A , Math.max(B, C)) == 0) {
